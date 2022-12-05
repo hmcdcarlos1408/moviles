3d17847d521aa6256a0366c688fb7a8d3abb3bef
TidyMetal56 <carlosvera_99@hotmail.com>
2022-12-05 13:30:19 -0600

    maaster

--- movil/App.js
+++ movil/App.js
@@ -12,20 +12,12 @@ import {
   VStack,
   Box,
 } from "native-base";
-<<<<<<< HEAD
-import PPP from "./screens/Principal";
-import Cal from "./screens/Calcular";
-import Info from "./screens/Informacion";
-=======
-
->>>>>>> 9e6b32a406a5bf6def515d50796b2fe799a1e312
-import NativeBaseIcon from "./components/NativeBaseIcon";
+//import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
 import PPP from "./screens/Principal";
 import Info from "./screens/Informacion";
 import Cal from "./screens/Calcular";
 
-<<<<<<< HEAD
 const App=() =>{
   return (
     <NativeBaseProvider>
@@ -39,14 +31,3 @@ const App=() =>{
 );
 }
 export default App;
-=======
-const App=()=>{
-  return (<NativeBaseProvider>
-    <PPP>
-      <Info></Info>
-<Cal></Cal>
-    </PPP> 
-  </NativeBaseProvider>)
-}
-export default App();
->>>>>>> 9e6b32a406a5bf6def515d50796b2fe799a1e312
