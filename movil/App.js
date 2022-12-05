0d0eb4e622a86ddf0908ac1adc08c59db40a95fd
Claudia-Gutierrez-Diaz <103269801+Claudia-Gutierrez-Diaz@users.noreply.github.com>
2022-12-05 14:17:58 -0600

    master

--- movil/App.js
+++ movil/App.js
@@ -1,39 +1,59 @@
 import React from "react";
-import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Box,
-} from "native-base";
-//import NativeBaseIcon from "./components/NativeBaseIcon";
-import { Platform } from "react-native";
-import PPP from "./screens/Principal";
-import Info from "./screens/Informacion";
-import Cal from "./screens/Calcular";
+import { ImageBackground, StyleSheet, Text, View, Button, SafeAreaView,Alert } from "react-native";
+import { CreateNativeStackNavigator } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import Boton from "./components/Boton";
+import Home from "./components/Home";
+import MainStack from "./navigation/MainStack";
 
 
-const App=() =>{
-  return (
-    <NativeBaseProvider>
-<<<<<<< HEAD
-   
-=======
-      <PPP/>
-      <br/>
-     
-      <Cal/>
-      <br/>
-      <Info/>
->>>>>>> 80b1215fb607a4db6a4b0d38115211075468a248
-    </NativeBaseProvider>
-    
+const image = { uri: "https://reactjs.org/logo-og.png" };
+const Separator = () => (
+  <View style={styles.separator} />
 );
-}
-export default App;
+
+function App(){
+  return(
+    <View style={styles.container}>
+      <SafeAreaView style={{ flex: 1}}>
+ 
+    
+     <MainStack />
+   
+    
+    </SafeAreaView> 
+  </View> 
+  );
+} 
+const styles=StyleSheet.create({
+  container:{
+  
+    backgroundColor:'#f0f0f0',
+    flex:1,
+    justifyContent: 'center'
+
+  },
+
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
+
+
+
+})
+export default  App;
